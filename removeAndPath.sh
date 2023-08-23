8ea326580d2051a8ea752420ad179e3d5cf00a85
Tobias Gurtzick <magic@wizardtales.com>
2023-08-23 17:14:25 +0200

    new hero home header adjustment
    Signed-off-by: Tobias Gurtzick <magic@wizardtales.com>

--- removeAndPath.sh
+++ removeAndPath.sh
@@ -8,6 +8,6 @@ git cherry-pick 5c922196e852ce64c1e73309887b2eda2bd187a3
 git cherry-pick 7e2187f62fe54f5df70812822fbd2b4114b96bca
 # git cherry-pick 46e46ebca1c8d0da5bd226f057f6e0826352fd92
 # sometimes fails
-git cherry-pick e2f053d6d608c3547703ff39006e5ce126612177
+git cherry-pick 6ded9b5a554263f1511aa1e1fafcebd44ae7d4c2
 
 # find theme -name *.html | xargs -I% bash -c "sed -i -e '/<link href=\"https:\/\/fonts.googleapis.com\" rel=\"preconnect\">\|<link href=\"https:\/\/fonts.gstatic.com\" rel=\"preconnect\" crossorigin=\"anonymous\">\|<script src=\"https:\/\/ajax.googleapis.com\/ajax\/libs\/webfont\/1.6.26\/webfont.js\" type=\"text\/javascript\"><\/script>\|<script type=\"text\/javascript\">WebFont.load(.*);<\/script>/d' % && sed -i 's/<meta content=\"Webflow\" name=\"generator\">/<meta property=\"og:image:type\" content=\"image\/jpeg\">\n<meta property=\"og:image:height\" content=\"500\">\n<meta property=\"og:image:width\" content=\"500\">\n<meta property=\"og:site_name\" content=\"WX-ONE\">\n<meta property=\"og:url\" content=\"https:\/\/pre.wx1.de\">\n<meta content=\"https:\/\/pre.wx1.de\/assets\/images\/WX-One_og.jpeg\" property=\"og:image\">\n<meta content=\"https:\/\/pre.wx1.de\/assets\/images\/WX-One_og.jpeg\" property=\"twitter:image\">/' %"
